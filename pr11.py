0776c787d2fc968c378c0c3f671f39b4a293f82b
ashishchovatiya <52528682+ashishchovatiya@users.noreply.github.com>
2020-01-01 10:23:34 +0530

    Add files via upload

---
+++ pr11.py
@@ -0,0 +1,7 @@
+a,b,c=map(int,input('enter a, b & c :').split(' '))
+d = (b**2) - (4*a*c)
+sol1 = (-b-(d**0.5))/(2*a)
+sol2 = (-b+(d**0.5))/(2*a)
+print('solutions are')
+print('sol1 = ',sol1)
+print('sol2 = ',sol2)
