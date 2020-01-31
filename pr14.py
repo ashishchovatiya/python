197c7694cb8de4d1963609ef5c7adc1807d0d6b8
ashishchovatiya <52528682+ashishchovatiya@users.noreply.github.com>
2020-01-31 16:32:52 +0530

    Update pr14.py

--- pr14.py
+++ pr14.py
@@ -1,16 +1,18 @@
 n=int(input('how many numbers you want : '));
-a=0
-b=1
-if n ==1:
-    print(0)
-elif n==0 or n<0:
-    print('please enter right number')
-else :
-    print(a);
-    print(b);
-    for i in range(2,n):
-       c=a+b
-       a=b
-       b=c
-       print(c)
+def fibo(n):
+    a=0
+    b=1
+    if n ==1:
+        print(0)
+    elif n==0 or n<0:
+        print('please enter right number')
+    else :
+        print(a);
+        print(b);
+        for i in range(2,n):
+           c=a+b
+           a=b
+           b=c
+           print(c)
 
+fibo(n)
